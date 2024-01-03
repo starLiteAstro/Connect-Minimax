c8ff6efd78b2d6df0810074999312f8cf9e07b69
Hao-Yen Tang <u2201755@live.warwick.ac.uk>
2024-01-03 00:53:21 +0000

    New repo

---
+++ player.py
@@ -0,0 +1,172 @@
+import board
+import random
+import math
+
+# The aim of this coursework is to implement the minimax algorithm to determine the next move for a game of Connect.
+# The goal in Connect is for a player to create a line of the specified number of pieces, either horizontally, vertically or diagonally.
+# It is a 2-player game with each player having their own type of piece, "X" and "O" in this instantiation.
+# You will implement the strategy for the first player, who plays "X". The opponent, who always goes second, plays "O".
+# The number of rows and columns in the board varies, as does the number of pieces required in a line to win.
+# Each turn, a player must select a column in which to place a piece. The piece then falls to the lowest unfilled location.
+# Rows and columns are indexed from 0. Thus, if at the start of the game you choose column 2, your piece will fall to row 0 of column 2. 
+# If the opponent also selects column 2 their piece will end up in row 1 of column 2, and so on until column 2 is full (as determined
+# by the number of rows). 
+# Note that board locations are indexed in the data structure as [row][column]. However, you should primarily be using checkFull(), 
+# checkSpace() etc. in board.py rather than interacting directly with the board.gameBoard structure.
+# It is recommended that look at the comments in board.py to get a feel for how it is implemented. 
+#
+# Your task is to complete the two methods, 'getMove()' and 'getMoveAlphaBeta()'.
+#
+# getMove() should implement the minimax algorithm, with no pruning. It should return a number, between 0 and (maxColumns - 1), to
+# select which column your next piece should be placed in. Remember that columns are zero indexed, and so if there are 4 columns in
+# you must return 0, 1, 2 or 3. 
+#
+# getMoveAlphaBeta() should implement minimax with alpha-beta pruning. As before, it should return the column that your next
+# piece should be placed in.
+#
+# The only imports permitted are those already imported. You may not use any additional resources. Doing so is likely to result in a 
+# mark of zero. Also note that this coursework is NOT an exercise in Python proficiency, which is to say you are not expected to use the
+# most "Pythonic" way of doing things. Your implementation should be readable and commented appropriately. Similarly, the code you are 
+# given is intended to be readable rather than particularly efficient or "Pythonic".
+#
+# IMPORTANT: You MUST TRACK how many nodes you expand in your minimax and minimax with alpha-beta implementations.
+# IMPORTANT: In your minimax with alpha-beta implementation, when pruning you MUST TRACK the number of times you prune.
+class Player:
+
+	def __init__(self, name):
+		self.name = name
+		self.numExpanded = 0 # Use this to track the number of nodes you expand
+		self.numPruned = 0 # Use this to track the number of times you prune
+		self.limit = 10000 # Use this to limit the number of nodes for iterative deepening
+		self.startDepth = 1 # Use this to track the starting depth for iterative deepening
+
+	def getMove(self, gameBoard):
+		if self.name == 'X':
+			return self.minimax(gameBoard, -1, True)[0] # Set depth to -1 to run a full search (no depth cutoff)
+		else:
+			return self.minimax(gameBoard, -1, False)[0] # For player 2 minimax AI
+
+	def getMoveAlphaBeta(self, gameBoard):
+		if self.name == 'X':
+			return self.minimaxAB(gameBoard, -1, True, -math.inf, math.inf)[0] # Set depth to -1 to run a full search (no depth cutoff)
+		else:
+			return self.minimaxAB(gameBoard, -1, False, -math.inf, math.inf)[0] # For player 2 minimaxAB AI
+
+	def minimax(self, gameBoard, depth, maxingPlayer):
+		if depth == 0 or gameBoard.checkWin():
+			if gameBoard.lastPlay[2] == 'X':
+				if depth <= 0:
+					return None, 1 # Return 1 for maximising player
+				else:
+					return None, 1 / depth # Reduce score by depth to encourage faster wins
+			else:
+				if depth <= 0:
+					return None, -1
+				else:
+					return None, -1 / depth
+		if gameBoard.checkFull():
+			return None, 0
+		
+		maxCol = gameBoard.numColumns
+		maxRow = gameBoard.numRows
+		colOrder = []
+		for i in range(maxCol):
+			colOrder.append(math.ceil(maxCol // 2 + (1 - 2 * (i % 2)) * (i + 1) // 2))
+		if depth > 0:
+			depth = depth - 1
+
+		if maxingPlayer:
+			maxEval = -math.inf
+			column = random.randint(0, maxCol - 1)
+			for col in colOrder:
+				if gameBoard.colFills[col] < maxRow:
+					self.numExpanded += 1
+					temp = gameBoard.copy()
+					temp.addPiece(col, 'X')
+					eval = self.minimax(temp, depth, False)[1]
+					if eval > maxEval:
+						column = col
+					maxEval = max(maxEval, eval)
+			return column, maxEval
+		else:
+			minEval = math.inf
+			column = random.randint(0, maxCol - 1)
+			for col in range(maxCol):
+				if gameBoard.colFills[col] < maxRow:
+					self.numExpanded += 1
+					temp = gameBoard.copy()
+					temp.addPiece(col, 'O')
+					eval = self.minimax(temp, depth, True)[1]
+					if eval < minEval:
+						column = col
+					minEval = min(minEval, eval)
+			return column, minEval
+
+	def minimaxAB(self, gameBoard, depth, maxingPlayer, alpha, beta):
+			#print("Depth", depth, "maxingPlayer", maxingPlayer, "alpha", alpha, "beta", beta)
+			#gameBoard.printBoard()
+			if depth == 0 or gameBoard.checkWin():
+				if gameBoard.lastPlay[2] == 'X':
+					if depth <= 0:
+						return None, 1 # Return 1 for maximising player
+					else:
+						return None, 1 / depth # Reduce score by depth to encourage faster wins
+				else:
+					if depth <= 0:
+						return None, -1
+					else:
+						return None, -1 / depth
+
+			if gameBoard.checkFull():
+					return None, 0
+
+			maxCol = gameBoard.numColumns
+			maxRow = gameBoard.numRows
+			colOrder = []
+			for i in range(maxCol):
+					colOrder.append(math.ceil(maxCol // 2 + (1 - 2 * (i % 2)) * (i + 1) // 2))
+			if depth > 0:
+					depth = depth - 1
+
+			if maxingPlayer:
+				maxEval = -math.inf
+				column = random.randint(0, maxCol - 1)
+				while self.numExpanded < self.limit:
+					for col in colOrder:
+						if gameBoard.colFills[col] < maxRow:
+							self.numExpanded += 1
+							temp = gameBoard.copy()
+							temp.addPiece(col, 'X')
+							eval = self.minimaxAB(temp, depth, False, alpha, beta)[1]
+							if eval > maxEval:
+								#print("MAX: Old column", column, ", new column", col, "depth", depth)
+								column = col
+							maxEval = max(maxEval, eval)
+							#print("MaxEval", maxEval, "alpha", alpha, "beta", beta)
+							alpha = max(alpha, maxEval)
+							if beta <= alpha:
+								self.numPruned += 1
+								break
+					depth += 1
+				return column, maxEval
+			else:
+				minEval = math.inf
+				column = random.randint(0, maxCol - 1)
+				while self.numExpanded < self.limit:
+					for col in range(maxCol):
+						if gameBoard.colFills[col] < maxRow:
+							self.numExpanded += 1
+							temp = gameBoard.copy()
+							temp.addPiece(col, 'O')
+							eval = self.minimaxAB(temp, depth, True, alpha, beta)[1]
+							if eval < minEval:
+								#print("MIN: Old column", column, ", new column", col, "depth", depth)
+								column = col
+							minEval = min(minEval, eval)
+							#print("MinEval", minEval, "alpha", alpha, "beta", beta)
+							beta = min(beta, minEval)
+							if beta <= alpha:
+								self.numPruned += 1
+								break
+					depth += 1
+				return column, minEval
---
+++ runGame.py
@@ -0,0 +1,62 @@
+import board
+import game
+import player
+import randomPlayer
+# Note that you can comment out the following if you don't want to seed the random player differently each run
+from datetime import datetime
+import time
+
+games = 1
+wins = 0
+draws = 0
+losses = 0
+start = time.time()
+
+for i in range(games):
+    # This script allows you to test your solution.
+    # Your coursework implementation must always be player 1.
+    # You should consider changing player 2 to use a minimax approach for evaluation.
+    # It is recommended that you also consider other game board sizes, and vary the number of pieces 
+    # that are required in a line to win. There are examples of the method calls to create such games
+    # commented out below.
+    p1 = player.Player("X")
+
+    # Player 2 currently picks random moves and so, while player 2 is not very good, it does allow you to
+    # start testing your solution. Once you have something sensible, you should change player 2 to be more 
+    # intelligent. Note that you can specify a seed for the random player (currently the seed is '42'),
+    # which allows for testing in a consistent environment.
+    # Note that the following two lines seed the random player differently each run
+    seed = datetime.now().timestamp()
+    # p2 = player.Player("O")
+    # Instead of randomly seeding, you can comment out the following line to seed the random player and
+    # test with a consistent opponent
+    # p2 = randomPlayer.RandomPlayer("O", seed)
+    p2 = randomPlayer.userPlayer("O")
+
+    # The arguments to game.Game specify the two players, the number of rows, the number of columns
+    # and the number of pieces that need to be placed in a line in order to win.
+    # g = game.Game(p1, p2, 5, 6, 4)
+    # g = game.Game(p1, p2, 5, 6, 3)
+    # g = game.Game(p1, p2, 4, 5, 3)
+    # g = game.Game(p1, p2, 4, 4, 4)
+    # g = game.Game(p1, p2, 4, 4, 3)
+    g = game.Game(p1, p2, 6, 7, 4)
+
+# You can pass 'True' to the playGame() method to test your alpha-beta pruning approach, i.e., to make
+# player 1 use alpha-beta. If you want player 2 to use alpha-beta you will need to ensure 
+# that you create player 2 accordingly. 
+    val = g.playGame(True)
+    if val == 1:
+        wins += 1
+    elif val == -1:
+        losses += 1
+    else:
+        draws += 1
+
+end = time.time()
+print(f"Runtime (ms): {(end - start) * 10**3} ms")
+print(f"Runtime (s): {(end - start)} s")
+print(f"Games: {games}")
+print(f"Wins: {wins} ({wins / games * 100}%)")
+print(f"Draws: {draws} ({draws / games * 100}%)")
+print(f"Losses: {losses} ({losses / games * 100}%)")
